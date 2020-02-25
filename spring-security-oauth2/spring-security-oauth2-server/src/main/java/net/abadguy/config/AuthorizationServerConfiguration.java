86c69952bd962b62c256265f134521c10fad4bc2
ABadGuy <abadguy@abadguy.net>
2020-02-25 13:15:52 +0800

    ouath基于内存

---
+++ spring-security-oauth2/spring-security-oauth2-server/src/main/java/net/abadguy/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,27 @@
+package net.abadguy.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+import javax.xml.ws.Action;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret(bCryptPasswordEncoder.encode("secret"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("app")
+                .redirectUris("http://baidu.com");
+    }
+}
